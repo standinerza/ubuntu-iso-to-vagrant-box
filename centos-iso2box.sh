9a3e5b102d8d69ab810c9318bfdb732e43a3a1c0
Farhan <standin.erza@gmail.com>
2020-05-07 17:17:12 +0700

    add dependencies package

--- centos-iso2box.sh
+++ centos-iso2box.sh
@@ -12,7 +12,7 @@ for i in /root /home/vagrant; do
   wget --no-check-certificate https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant -O $i/.ssh/id_rsa;
   wget --no-check-certificate https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub -O $i/.ssh/id_rsa.pub;
   chmod 600 $i/.ssh/id_*;
-  cat $i/.ssh/id_rsa.pub > $U/.ssh/authorized_keys;
+  cat $i/.ssh/id_rsa.pub > $i/.ssh/authorized_keys;
 done
 
 #Change ownership of the key file 
@@ -24,7 +24,7 @@ echo 'vagrant ALL=(ALL) NOPASSWD: ALL' >>/etc/sudoers
 #Reboot the server
 
 #Install kernel-header and wget 
-yum -y install vim kernel-devel wget
+yum -y install vim kernel-devel wget bzip2 tar gcc 
 
 #Mount VirtualBox Guest and Install it 
 mount /dev/cdrom /mnt
@@ -35,7 +35,7 @@ yum -y install yum-utils
 package-cleanup -y --oldkernels --count=1
 yum -y autoremove
 yum -y remove yum-utils 
-yum clean
+yum clean all
 rm -rf /tmp/*
 rm -f /var/log/wtmp /var/log/btmp
 dd if=/dev/zero of=/EMPTY bs=1M
